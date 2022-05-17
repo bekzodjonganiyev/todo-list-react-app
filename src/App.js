5368d3181a5d0a4ae4d144563676912772d1bc4c
bezodjonganiyev <bekzodganiyev471@gmail.com>
2022-05-17 16:02:17 +0500

    WIP

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useRef, useState } from "react"
+import { useState } from "react"
 import TodoItem from "./Components/TodoItem"
 import "./Assets/main.scss"
 
 function App() {
-  const [allTodos, setAllTodos] = useState(
+  const [allTodos] = useState(
     JSON.parse(window.localStorage.getItem("todos")) || []
   )
-    const [todos , setTodos] = useState(allTodos)
-    const [defCheck , setDefCheck] = useState(true)
+  const [todos, setTodos] = useState(allTodos)
+  const [defCheck, setDefCheck] = useState(true)
 
   const handleInput = evt => {
     const newTodo = {
@@ -46,18 +46,21 @@ function App() {
     window.localStorage.setItem("todos", JSON.stringify([...todos]))
   }
 
-  const handleRadio = (evt) =>{
+  const handleRadio = (evt) => {
 
-    if(evt.target.id === 'complated'){
-      const  searchTodo  = allTodos.filter((item)=>item.isCompleted === true)
+    if (evt.target.id === 'complated') {
+      const searchTodo = allTodos.filter((item) => item.isCompleted === true)
       setTodos(searchTodo)
       console.log(todos);
       setDefCheck(false)
-    }else if(evt.target.id === 'all'){
+      window.localStorage.setItem("todos", JSON.stringify(searchTodo))
+
+
+    } else if (evt.target.id === 'all') {
       setTodos(allTodos)
-      setDefCheck((prev)=>!prev)
-    }else if(evt.target.id === 'active'){
-      const  searchTodo  = allTodos.filter((item)=>item.isCompleted === false)
+      setDefCheck((prev) => !prev)
+    } else if (evt.target.id === 'active') {
+      const searchTodo = allTodos.filter((item) => item.isCompleted === false)
       console.log(searchTodo);
       setTodos(searchTodo)
       setDefCheck(false)
@@ -68,29 +71,145 @@ function App() {
   return (
     <>
       <h1>Todo List with Reac Js</h1>
+
       <input className="input" onKeyUp={handleInput} type="text" placeholder="Todo..." />
       <div>
+
         <label className="radioLabel" htmlFor="all"  >All</label>
-        <input type={'radio'} className='inputRadio' name = 'todos'  id="all" checked = {defCheck} onClick={handleRadio} />
+        <input type={'radio'} className='inputRadio' name='todos' id="all" checked={defCheck} onClick={handleRadio} />
+
         <label className="radioLabel" htmlFor="active" >active</label>
-        <input type={'radio'} className='inputRadio' name = 'todos' id="active"  onClick={handleRadio}/>
-        <label className="radioLabel" htmlFor="complated" >complated</label>
-        <input type={'radio'} className='inputRadio' name = 'todos' id="complated" onClick={handleRadio}/>
+        <input type={'radio'} className='inputRadio' name='todos' id="active" onClick={handleRadio} />
+
+        <label className="radioLabel" htmlFor="complated" >Complated</label>
+        <input type={'radio'} className='inputRadio' name='todos' id="complated" onClick={handleRadio} />
+
       </div>
       <ul>
         {todos.map(item => (
           <TodoItem
             handleComplate={handleComplate}
             handleDeleteTodo={handleDeleteTodo}
-            isComplated={item.isCompleted}
+            isCompleted={item.isCompleted}
             key={item.id}
             id={item.id}
             title={item.title}>
-            </TodoItem>
+          </TodoItem>
         ))}
       </ul>
     </>
   )
 }
-
 export default App
+
+
+
+// import { useState, useRef } from "react";
+
+// import "./Assets/main.scss"
+
+// function App() {
+
+//   const [state, setState] = useState(JSON.parse(window.localStorage.getItem("todos")) || [])
+//   const elInput = useRef()
+
+//   const formSubmit = (e) => {
+//     e.preventDefault()
+
+//     const newItem = {
+//       id: new Date().getTime(),
+//       title: elInput.current.value,
+//       isCompleted: false
+//     }
+
+//     window.localStorage.setItem("todos", JSON.stringify([...state, newItem]))
+//     setState([...state, newItem])
+
+//   }
+
+//   const deleteItem = (e) => {
+//     const deletedItemId = e.target.dataset.todoId - 0
+
+//     const filteredItems = state.filter(item => item.id !== deletedItemId)
+
+//     window.localStorage.removeItem("todos")
+//     setState(filteredItems)
+//   }
+
+//   const handleIsCheck = (e) => {
+//     const isCheckedId = e.target.dataset.todoId - 0;
+
+//     const findedItem = state.find(item => item.id === isCheckedId)
+// //     console.log(findedItem);
+// //     const data = new Date(1652769393373)
+
+// // console.log(data.getHours());
+// //     console.log(findedItem.isCompleted );
+
+//     findedItem.isCompleted = !findedItem.isCompleted
+//     // console.log(findedItem.isCompleted );
+
+//     setState([...state])
+//     window.localStorage.setItem("todos", JSON.stringify([...state]))
+
+//   }
+
+//   return (
+//     <>
+//       <h1 className="todo-title">Todo List</h1>
+
+//       <div className="btns">
+//         <button className="btn all-btn">All</button>
+//         <button className="btn active-btn">Active</button>
+//         <button className="btn complate-btn">Complate</button>
+//       </div>
+
+//       <form className="form" onSubmit={formSubmit}>
+
+//         <input ref={elInput} type="text" className="input" required />
+//         <button type="submit" className="btn submit-btn">Add</button>
+
+//         <ul style={{ padding: "0 10px" }}>
+//           {
+//             state.map(item => (
+//               <li
+//                 key={item.id}
+//                 data-todo-id={item.id}
+//                 style={{
+//                   display: "flex",
+//                   alignItems: "center",
+//                   justifyContent: "space-between",
+//                   margin: "30px auto",
+//                   backgroundColor: "hsl(206, 33%, 96%)",
+//                   boxSizing: "border-box",
+//                   padding: "5px 10px",
+//                   borderRadius: "10px"
+//                 }}>
+//                 <input
+//                   type="checkbox"
+//                   onChange={handleIsCheck}
+//                   data-todo-id={item.id}
+//                   style={{
+
+//                   }}
+//                 />
+//                 <p
+//                   className={item.isCompleted === true ? "complated" : "uncomplated"}
+//                   style={{
+//                     color: "hsl(209, 61%, 16%)",
+//                     fontSize: "30px",
+//                     fontWeight: "800",
+//                     margin: "5px 0px",
+//                   }}>{item.title}</p>
+//                 <button type="button" className="btn delete-btn" onClick={deleteItem} data-todo-id={item.id}>Delete</button>
+//               </li>
+//             ))
+//           }
+//         </ul>
+
+//       </form>
+//     </>
+//   )
+// }
+
+// export default App
